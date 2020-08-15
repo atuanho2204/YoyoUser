6e1c96d51e20a4c487e537da77b663fd34b71ead
atuanho2204 <itto22041999@gmail.com>
2020-08-15 18:47:34 -0400

    add roles api

---
+++ backend/App/endpoints/roles.js
@@ -0,0 +1,4 @@
+const auth = require('../authentication')
+const connection = require('../database') // connected db
+const express = require('express')
+const router = express.Router() // express route handler
