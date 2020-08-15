9eab1b48f6baf569324f83d84266d65d0479a0cc
atuanho2204 <itto22041999@gmail.com>
2020-08-15 18:53:05 -0400

    create router for role

--- backend/App/routes.js
+++ backend/App/routes.js
@@ -1,6 +1,8 @@
 module.exports = function(app) {
     const loginRoute = require('./endpoints/login');
+    const roleRoute = require('./endpoints/roles');
 
     app.use('/api/login', loginRoute);
+    app.use('/api/roles', roleRoute);
 
 }
