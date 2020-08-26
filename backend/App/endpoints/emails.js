6245e86a522c727391ee09de8df42c2ee6a75466
atuanho2204 <itto22041999@gmail.com>
2020-08-26 12:47:28 -0400

    fixed emails endpoints, add route

--- backend/App/endpoints/emails.js
+++ backend/App/endpoints/emails.js
@@ -45,3 +45,59 @@ router.post('/new', (req, res) => {
         }
     )
 })
+
+router.put('/', (req, res) => {
+    if (!auth(req.headers)) {
+        res.status(401).json({
+            response: "Authentication failed",
+        });
+        return;
+    }
+
+    let email = req.body;
+
+    connection.query(
+        `UPDATE emails
+        SET firstName = '${email.firstName}',
+            lastName = '${email.lastName}',
+            email = '${email.email}'
+        WHERE id = ${email.id};`,
+        (err, rows, field) => {
+            if(err) {
+                res.status(400).end();
+                return;
+            }
+            res.status(200).json({
+                msg: 'Update successfully'
+            });
+        }
+    )
+})
+
+
+
+router.delete('/:id', (req, res) => {
+    if (!auth(req.headers)) {
+        res.status(401).json({
+            response: "Authentication failed",
+        });
+        return;
+    }
+
+    let id = req.params.id;
+
+    connection.query(
+        `DELETE FROM emails
+            WHERE id = '${id}';`,
+
+        (err, rows, field) => {
+            if(err) {
+                res.status(400).end();
+                return;
+            }
+            res.status(200).end()
+        }
+    )
+})
+
+module.exports = router 
