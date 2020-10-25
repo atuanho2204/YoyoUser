33f6f73bbc10dc297764349162d4d60044b19a82
aho71 <aho71@gatech.edu>
2020-10-25 18:43:42 -0400

    implement get User ID

--- backend/App/endpoints/users.js
+++ backend/App/endpoints/users.js
@@ -4,7 +4,36 @@ const express = require('express')
 const router = express.Router() // express route handler 
 
 
+router.get('/:id', (req, res) => {
+    if (!auth(req.headers)) {
+        res.status(401).json({
+            response: "Authentication failed",
+        });
+        return;
+    }
 
+    let id = req.params.id;
+
+    connection.query(
+        `SELECT id, username, firstName, lastName, email, roleMask FROM users
+            WHERE id = '${id}';`,
+
+        (err, rows, field) => {
+            if(err) {
+                res.status(400).end();
+                return;
+            }
+            if (rows.length == 0) {
+                res.status(401).json({
+                    msg: "Invalid id",
+                })
+            }
+            else {
+                res.status(200).json(rows[0])
+            }
+        }
+    )
+})
 
 
 
