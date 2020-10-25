7d9d9be119accc2a8bd064731bd856d4b480d774
aho71 <aho71@gatech.edu>
2020-10-25 18:19:18 -0400

    fix conflict of email table

--- frontend/src/views/admin/EmailTable.jsx
+++ frontend/src/views/admin/EmailTable.jsx
@@ -51,48 +51,7 @@ export class EmailTable extends React.Component {
     
     return (
       <>
-      <NavBar />
-      {this.state.showModal 
-        && <EmailDialog email={this.state.curEmail} 
-                        handleHideModal={() => this.handleHideModal()}
-                        update={(email) => this.updateEmail(email)} />}
-                      
-      <div className="m-2 ml-4 mr-4">
-          <h3 className="d-inline">Email Table</h3>
-          <button className="btn btn-primary float-right"
-                  onClick={() => this.addEmail()}>Create Email</button>
-          <hr />
-          <table className="table table-striped table-bordered">
-            <thead>
-              <tr>
-              {
-              this.headers.map((item, id) => (
-                <th scope="col" key={id}>{item}</th>
-              ))
-              }
-              </tr>
-            </thead>
-            <tbody>
-              {
-              this.state.emails.map((email, id) => (
-                <tr key={id}>
-                  <td>{email.firstName}</td>
-                  <td>{email.lastName}</td>
-                  <td>{email.email}</td>
-                  <td><button style={{fontSize:"0.6em"}}
-                              className="btn btn-sm btn-info w-50"
-                              onClick={() => this.openEditor(email)}> Edit </button></td>
-                  <td><button style={{fontSize:"0.6em"}}
-                              className="btn btn-sm btn-danger w-50"
-                              onClick={() => this.deleteEmail(email.email, email.id)}> Delete </button></td>
-                </tr>
-              ))
-              }
-            </tbody>
-          </table>
-        </div>
 
-      </>
     )
   }
   componentDidMount() {
