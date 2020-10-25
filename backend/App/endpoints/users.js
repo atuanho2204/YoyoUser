12d6f09cf322f2982d5b33437ca16467ff492f57
aho71 <aho71@gatech.edu>
2020-10-25 18:42:45 -0400

    add users endpoints

---
+++ backend/App/endpoints/users.js
@@ -0,0 +1,18 @@
+const auth = require('../authentication')
+const connection = require('../database') // connected db 
+const express = require('express')
+const router = express.Router() // express route handler 
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router 
